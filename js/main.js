cc42de5af4991a89d08e7b50779dfc34d5219da6
Abdullah H. AlShaikh <Abdullah@AlShai5.com>
2021-09-04 01:16:00 +0300

    Minor fixes.

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ const sections = {
 
 document.querySelectorAll('.top-header > a:first-child, .top-header .nav-link, .sidebar .nav-link').forEach(section => {
   section.addEventListener('click', event => {
-    console.log(event.currentTarget)
     const target = event.currentTarget.getAttribute('href')?.replace('/#', '')
     if (!sections[target]) { return }
     event.preventDefault()
