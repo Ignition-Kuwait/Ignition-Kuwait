7c728bd356bae78aa15a6b1150a7c91ac3171c84
Abdullah H. AlShaikh <Abdullah@AlShai5.com>
2021-09-04 00:52:30 +0300

    Reinstated sidebar and made necessary changes/fixes.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-// const sidebarButton = document.getElementById('sidebarButton')
+const sidebarButton = document.getElementById('sidebarButton')
 
-// sidebarButton.addEventListener('click', event => {
-//   document.body.classList.toggle('show-sidebar')
-// })
+sidebarButton.addEventListener('click', event => {
+  document.body.classList.toggle('show-sidebar')
+})
 
 const mainWrapper = document.querySelector('main.page-content .wrapper')
 const sections = {
+  home: document.getElementById('home'),
   about: document.getElementById('about'),
   mission: document.getElementById('mission'),
   contact: document.getElementById('contact')
 }
 
-
-// TODO: Add sidebar links.
-document.querySelectorAll('.top-header .nav-link').forEach(section => {
+document.querySelectorAll('.top-header > a:first-child, .top-header .nav-link, .sidebar .nav-link').forEach(section => {
   section.addEventListener('click', event => {
+    console.log(event.currentTarget)
     const target = event.currentTarget.getAttribute('href')?.replace('/#', '')
     if (!sections[target]) { return }
     event.preventDefault()
